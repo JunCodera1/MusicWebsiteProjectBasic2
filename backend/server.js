309aab9284d5d79b348833d5b51bab3edc765bc8
JunCodera1 <minhtien0601200@gmail.com>
2024-10-12 15:18:23 +0700

    Mongoose config

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 
-app.listen(5000, () =>{
-    console.log("Server started at https://localhost:5000 hello");
+app.get("/", (req, res) => {});
+
+console.log(process.env.MONGODB_URI);
+
+app.listen(5000, () => {
+  console.log("Server started at https://localhost:5000 hello");
 });
