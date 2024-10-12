0b42ec25ee1bb13e64fb8a55a546daa08eb21cad
JunCodera1 <minhtien0601200@gmail.com>
2024-10-12 18:27:36 +0700

    mongodb setup

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
+
+export const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1); // Exit process with failure
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (req, res) => {});
-
 console.log(process.env.MONGODB_URI);
 
+
 app.listen(5000, () => {
-  console.log("Server started at https://localhost:5000 hello");
+  connectDB();
+  console.log("Server started at http://localhost:5000");
 });
