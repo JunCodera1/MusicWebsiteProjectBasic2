eb91ab9e898fc7bf097d16fd10e39b0b13e17e28
JunCodera1 <minhtien0601200@gmail.com>
2024-10-13 23:47:44 +0700

    require API body

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,35 @@ import { connectDB } from "./config/db.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {});
+
+app.use(express.json()); // allow us to accept JSON data in the req.body
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body; // user will send this data
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all fields"
+    });
+  }
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create product: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// Postman desktop app
+
 console.log(process.env.MONGODB_URI);
-
-
-app.listen(5000, () => {
-  connectDB();
+connectDB();
+app.listen(PORT, () => {
   console.log("Server started at http://localhost:5000");
 });
