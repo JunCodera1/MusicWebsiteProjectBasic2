eb207ce7170777d4cbd3d0075273b42ed9a33929
JunCodera1 <minhtien0601200@gmail.com>
2024-10-26 00:10:19 +0700

    Remove songSchema

--- backend/model/product.model.js
+++ backend/model/product.model.js
@@ -20,53 +20,7 @@ const productSchema = new mongoose.Schema(
   }
 );
 
-const songSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  author_name: {
-    type: String,
-    required: true,
-  },
-  img: {
-    type: String,
-    required: true,
-  },
-  timesPlayed: {
-    type: Number,
-    required: true,
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  musicName: {
-    type: String,
-    required: true,
-  },
-  attribution: {
-    song: {
-      type: String,
-      required: true,
-    },
-    musicBy: {
-      type: String,
-      required: true,
-    },
-    download: {
-      type: String,
-      required: true,
-    },
-    stream: {
-      type: String,
-      required: true,
-    },
-  },
-});
 
 const Product = mongoose.model("Product", productSchema);
-const Song = mongoose.model("Song", songSchema);
 
 export default Product;
-export { Song };
