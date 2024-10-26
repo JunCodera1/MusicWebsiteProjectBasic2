21c3a881f80a085a4fdd3f4a4ca5106354cf07d0
JunCodera1 <minhtien0601200@gmail.com>
2024-10-26 09:17:35 +0700

    modify server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
+// import path from "path";
 
 import { connectDB } from "./config/db.js";
-
-import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -15,7 +13,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // allow us to accept JSON data in the req.body
 
-app.use("/api/products", productRoutes);
 
 // if(process.env.NODE_ENV === "production"){
 //   app.use(express.static(path.join(__dirname, "/frontend/dist")));
