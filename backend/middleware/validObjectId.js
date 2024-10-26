eeac465b3353e3b24ea64b64d24252c845235f8d
JunCodera1 <minhtien0601200@gmail.com>
2024-10-26 22:50:47 +0700

    Update: Comments in /midware/validObjectId.js

--- backend/middleware/validObjectId.js
+++ backend/middleware/validObjectId.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 module.exports = (req, res, next) => {
   if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    // if id is not valid
     return res.status(404).send({ message: "Invalid ID." });
   }
-  next();
+  next(); // if id is valid
 };
