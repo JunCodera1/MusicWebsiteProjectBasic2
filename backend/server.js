c97201521e27787196973152a0f38af80231aec6
JunCodera1 <minhtien0601200@gmail.com>
2024-10-26 19:55:37 +0700

    add auth in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/users", userRoutes);
+app.use("/api/login", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 console.log(process.env.MONGODB_URI);
