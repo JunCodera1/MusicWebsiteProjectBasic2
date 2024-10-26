fcec8a606cc1df5a8bf114c98b03195879c09713
JunCodera1 <minhtien0601200@gmail.com>
2024-10-26 22:49:25 +0700

    Update: Comments in admin.js

--- backend/middleware/admin.js
+++ backend/middleware/admin.js
@@ -5,19 +5,21 @@ module.exports = (req, res, next) => {
   if (!token) {
     return res
       .status(400)
-      .send({ message: "Access denied. No token provided." });
+      .send({ message: "Access denied. No token provided." }); // if there is no token
   }
 
   jwt.verify(token, process.env.JWTPRIVATEKEY, (error, validToken) => {
+    /// verify token
     if (error) {
       return res.status(400).send({ message: "Invalid token." });
     } else {
       if (!validToken.isAdmin) {
+        // if user is not admin
         return res
           .status(403)
           .send({ message: "You don't have access this content." });
       }
-      req.user = validToken;
+      req.user = validToken; // if user is admin
       next();
     }
   });
