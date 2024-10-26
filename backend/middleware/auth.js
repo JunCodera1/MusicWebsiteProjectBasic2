3505cc3163bf7d61a59ce2cdaddb628a4b71d660
JunCodera1 <minhtien0601200@gmail.com>
2024-10-26 22:48:46 +0700

    Update: /midware/auth.js comments

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,15 +3,18 @@ import jwt from "jsonwebtoken";
 module.exports = (req, res, next) => {
   const token = req.header("x-auth-token");
   if (!token) {
+    // if there is no token
     return res
       .status(400)
       .send({ message: "Access denied. No token provided." });
   }
 
   jwt.verify(token, process.env.JWTPRIVATEKEY, (error, validToken) => {
+    /// verify token
     if (error) {
-      return res.status(400).send({ message: "Invalid token." });
+      return res.status(400).send({ message: "Invalid token." }); // if token is invalid
     } else {
+      // if token is valid
       req.user = validToken;
       next();
     }
