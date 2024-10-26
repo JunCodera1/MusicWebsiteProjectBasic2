ef5672d7d9fe0177e77a81f86e3ee0e31a861751
JunCodera1 <minhtien0601200@gmail.com>
2024-10-26 22:18:10 +0700

    Add: Midware/auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+module.exports = (req, res, next) => {
+  const token = req.header("x-auth-token");
+  if (!token) {
+    return res
+      .status(400)
+      .send({ message: "Access denied. No token provided." });
+  }
+
+  jwt.verify(token, process.env.JWTPRIVATEKEY, (error, validToken) => {
+    if (error) {
+      return res.status(400).send({ message: "Invalid token." });
+    } else {
+      req.user = validToken;
+      next();
+    }
+  });
+};
