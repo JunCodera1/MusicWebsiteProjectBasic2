37dc96925d0b9b20d053a6a2cfcab144c349eeb9
JunCodera1 <minhtien0601200@gmail.com>
2024-10-27 20:51:30 +0700

    Added: songs route in server.js to use.

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
+import songRoutes from "./routes/songs.js";
 
 dotenv.config(); // Load environment variables from .env file
 const app = express(); // Create Express app
@@ -13,6 +14,7 @@ app.use(cors()); // Enable CORS
 app.use(express.json()); // Parse JSON bodies
 app.use("/api/users", userRoutes); // User Routes
 app.use("/api/login", authRoutes); // Auth Routes
+app.use("/api/songs", songRoutes); // Song Routes
 
 const PORT = process.env.PORT || 5000; // Set port
 console.log(process.env.MONGODB_URI); // Log MongoDB URI
