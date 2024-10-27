496875cd6e629908cce322859ae9f2e1ce9ff17b
JunCodera1 <minhtien0601200@gmail.com>
2024-10-27 21:43:07 +0700

    Added: POST method to create a playlist in playlists.js.

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -12,4 +12,22 @@ import Joi from "joi";
 
 const router = express.Router();
 
+// Create playlist
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send({ message: error.details[0].message });
+
+  const user = await User.findById(req.user._id);
+  const playlist = await Playlist({
+    ...req.body,
+    user: user._id,
+  }).save();
+  user.playlists.push(playlist._id);
+  await user.save();
+
+  res
+    .status(201)
+    .send({ data: playlist, message: "Playlist created successfully" });
+});
+
 export default router;
