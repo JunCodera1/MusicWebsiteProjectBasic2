8273bc11f92bc45ca8a18c311b6d92b6ebdc90b1
JunCodera1 <minhtien0601200@gmail.com>
2024-10-27 23:59:10 +0700

    Added: GET method to search in search.js.

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -2,3 +2,19 @@ import express from "express";
 import { Song } from "../model/song";
 import { Playlist } from "../model/playlist";
 import auth from "../middleware/auth";
+
+router.get("/", auth, async (req, res) => {
+  const { search } = req.query.search;
+  if (search !== "") {
+    const songs = await Song.find({
+      name: { $regex: search, options: "i" },
+    }).limit(10);
+    const playlists = await Play.find({
+      name: { $regex: search, options: "i" },
+    }).limit(10);
+    const result = { songs, playlists };
+    res.status(200).send({ data: result });
+  } else {
+    res.status(200).send({});
+  }
+});
