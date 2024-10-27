64acb01908e1471dd7470d0d44f1950870b8043f
JunCodera1 <minhtien0601200@gmail.com>
2024-10-27 23:18:44 +0700

    Updated: Playlist routes in server.js.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import songRoutes from "./routes/songs.js";
+import playlistRoutes from "./routes/playlists.js";
 
 dotenv.config(); // Load environment variables from .env file
 const app = express(); // Create Express app
@@ -15,6 +16,7 @@ app.use(express.json()); // Parse JSON bodies
 app.use("/api/users", userRoutes); // User Routes
 app.use("/api/login", authRoutes); // Auth Routes
 app.use("/api/songs", songRoutes); // Song Routes
+app.use("/api/playlists", playlistRoutes); // Playlist Routes
 
 const PORT = process.env.PORT || 5000; // Set port
 console.log(process.env.MONGODB_URI); // Log MongoDB URI
