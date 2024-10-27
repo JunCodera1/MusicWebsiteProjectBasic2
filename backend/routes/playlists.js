874a76be414040f835ccc4814260d38e0409563e
JunCodera1 <minhtien0601200@gmail.com>
2024-10-27 21:54:19 +0700

    Added: PUT method to Edit playlist by ID in playlists.js.

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -30,4 +30,44 @@ router.post("/", auth, async (req, res) => {
     .send({ data: playlist, message: "Playlist created successfully" });
 });
 
+// Edit playlists by ID
+router.put("/:id", [validObjectId, auth], async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(1).required(),
+    desc: Joi.string().allow(""),
+    image: Joi.string().allow(""),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).send({ message: error.details[0].message });
+  }
+
+  try {
+    const playlist = await Playlist.findById(req.params.id);
+    if (!playlist) {
+      return res.status(404).send({ message: "Playlist not found" });
+    }
+
+    const user = await User.findById(req.user._id);
+    if (!user._id.equals(playlist.user)) {
+      return res
+        .status(403)
+        .send({ message: "You are not authorized to edit this playlist" });
+    }
+
+    // Update the playlist
+    playlist.name = req.body.name;
+    playlist.desc = req.body.desc;
+    playlist.image = req.body.image;
+    await playlist.save();
+
+    res.status(200).send({ data: playlist, message: "Playlist updated" });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "An error occurred while updating the playlist" });
+  }
+});
+
 export default router;
