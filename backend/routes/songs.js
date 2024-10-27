bed97f60da0fca6b12fe8e3adae44546ed66225a
JunCodera1 <minhtien0601200@gmail.com>
2024-10-27 20:18:54 +0700

    Added: POST method for creating a new song

--- backend/routes/songs.js
+++ backend/routes/songs.js
@@ -6,3 +6,12 @@ import admin from "../middleware/admin.js";
 import validObjectId from "../middleware/validObjectId.js";
 
 const router = express.Router();
+
+// Create song
+router.post("/", admin, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send({ message: error.details[0].message });
+
+  const song = await Song(req.body).save();
+  res.status(201).send({ data: song, message: "Song created successfully" });
+});
