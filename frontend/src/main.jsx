a80a1dd6459fb7bec4b0c0ca986825b5a9de5c1d
BlackNoir <nhathuy1172005@gmai.com>
2024-10-28 20:30:36 +0700

    ADD : Setup Router in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 // Import your publishable key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -14,9 +18,21 @@ const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-        <App />
-      </ClerkProvider>
+      <BrowserRouter>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+          <App />
+          <ToastContainer
+
+            position="bottom-center"
+            autoClose={2000}
+            hideProgressBar={true}
+            closeButton={false}
+            theme="colored"
+            icon={false}
+          />
+
+        </ClerkProvider>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
