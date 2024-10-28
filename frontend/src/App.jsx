f87696ea8a9e23200a3be34179dd326ab5422b6a
JunCodera1 <minhtien0601200@gmail.com>
2024-10-28 23:07:21 +0700

    Removed: nested Router component from App.js to prevent duplicate Router error.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
-import './index.css'; 
+import "./index.css";
 
 import Navbar from "./components/Navbar";
-
 
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import FeedPage from "./pages/FeedPage";
@@ -14,18 +13,16 @@ import UploadPage from "./pages/UploadPage";
 
 const App = () => {
   return (
-    <Router>
-      <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
-        <Navbar />
-        <Routes>
-          <Route path="/create" element={<CreatePage />}></Route>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/feed" element={<FeedPage/>}></Route>
-          <Route path="/library" element={<LibraryPage/>}></Route>
-          <Route path="/upload" element={<UploadPage/>}></Route>
-        </Routes>
-      </Box>
-    </Router>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
+      <Routes>
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/feed" element={<FeedPage />} />
+        <Route path="/library" element={<LibraryPage />} />
+        <Route path="/upload" element={<UploadPage />} />
+      </Routes>
+    </Box>
   );
 };
 
