b894a520c9602f2c5495ca4887ea2b8b5e712a77
JunCodera1 <minhtien0601200@gmail.com>
2024-11-01 13:44:28 +0700

    Updated: Size for SignInButton

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -11,14 +11,14 @@ const SignIn = () => {
   return (
     <header>
       <SignedOut>
-        <SignInButton>
+        <SignInButton className="sm">
           <button className="bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-500 hover:to-orange-500 px-4 py-2 rounded">
             Sign In
           </button>
         </SignInButton>
       </SignedOut>
       <SignedIn>
-        <Box className="h-6">
+        <Box className="h-6" size="sm">
           <UserButton
             appearance={{
               elements: {
