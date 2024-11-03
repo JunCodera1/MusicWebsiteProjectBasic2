bfed1a0cd67a9437dc783c56637c434f9d65a2e7
JunCodera1 <minhtien0601200@gmail.com>
2024-11-03 15:46:46 +0700

    Added: post method to login.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -55,4 +55,33 @@ router.post("/register", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  // Step 1: Get email and password sent by user from req.body
+  const { email, password } = req.body;
+
+  // Step 2: Check if a user with given email exists. If not, the credentials are invalid
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res.status(403).json({ err: "Invalid credentials" });
+  }
+
+  console.log(user);
+
+  // Step 3: If the user exists, check if the password is correct. If not, the credentials are invalid
+  // This is a tricky step. Why? Because we have stored the original password in a hashed form, which we cannot use to get back the password.
+  // I cannot do : if(password === user.password)
+  // bcrypt.compare enabled us to compare 1 password in plaintext(password from req.body) to a hashed password(the one in our db) securely
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  // This will return true if the password is correct
+  if (!isPasswordValid) {
+    return res.status(403).json({ err: "Invalid credentials" });
+  }
+
+  // Step 4: If the credentials are valid, we create a token and return it to the user
+  const token = await getToken(email, user);
+  const userToReturn = { ...user.toJSON(), token };
+  delete userToReturn.password;
+  return res.status(200).json(userToReturn);
+});
+
 export default router;
