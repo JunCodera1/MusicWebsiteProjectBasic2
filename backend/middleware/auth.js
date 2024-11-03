0406daa5f9b2bb8fa156efc79507bb9a8903d890
JunCodera1 <minhtien0601200@gmail.com>
2024-11-03 15:01:11 +0700

    Updated helpers.js to ES6/7 syntax, and update route register auth.js.

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,24 +1,58 @@
-import jwt from "jsonwebtoken";
+import express from "express";
+import { User } from "/home/jun/Documents/MusicWebsiteProjectBasic2/backend/model/user.js";
+import bcrypt from "bcrypt";
+import getToken from "/home/jun/Documents/MusicWebsiteProjectBasic2/backend/utils/helpers.js";
 
-const auth = (req, res, next) => {
-  const token = req.header("x-auth-token");
-  if (!token) {
-    // if there is no token
-    return res
-      .status(400)
-      .send({ message: "Access denied. No token provided." });
-  }
+const router = express.Router();
 
-  jwt.verify(token, process.env.JWTPRIVATEKEY, (error, validToken) => {
-    /// verify token
-    if (error) {
-      return res.status(400).send({ message: "Invalid token." }); // if token is invalid
-    } else {
-      // if token is valid
-      req.user = validToken;
-      next();
+// This POST route will help to register a user
+router.post("/register", async (req, res) => {
+  //This code is run when the /register api is called as POST request
+
+  // My req.body will be of the format {email, password, firstname , lastname, username}
+  const { email, password, firstName, lastName, username } = req.body;
+
+  try {
+    //Step 2: Does a user with this email already exist?
+    const user = await User.findOne({ email: email });
+    if (user) {
+      //Status code by default 200
+      return res
+        .status(403)
+        .json({ error: "A user with this email already exists" });
     }
-  });
-};
 
-export default auth;
+    // This is valid request
+
+    // Step 3: Create a new user in DB
+    // Step 3.1: We do not store passwords in plain text.
+    // xyz: we convert password to hash.
+    // xyz -> abifjweoifjweifjifjweiofwejfiowef
+    // my hash of xyz depends on 2 parameters
+    // If I keep those 2 parameters same, xyz ALWAYS gives the same hash.
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUserData = {
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+      username,
+    };
+    const newUser = await User.create(newUserData);
+    console.log(newUserData);
+
+    // Step 4: We want to create the token to return to the user
+    const token = await getToken(email, newUser);
+
+    // Step 5: Return the result to the user
+    const userToReturn = { ...newUser.toJSON(), token };
+    console.log(userToReturn);
+    delete userToReturn.password;
+    return res.status(200).json(userToReturn);
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+export default router;
--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,7 +1,9 @@
 import jwt from "jsonwebtoken";
 
-exports.getToken = async (email, user) => {
+const getToken = async (email, user) => {
   // Assume this code is complete
   const token = jwt.sign({ identifier: user._id }, process.env.JWT_SECRET);
   return token;
 };
+
+export default getToken;
