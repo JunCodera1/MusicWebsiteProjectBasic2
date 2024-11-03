c0b0d70208f7e7ba89a662a96e153f7b4971112f
JunCodera1 <minhtien0601200@gmail.com>
2024-11-03 15:57:45 +0700

    Edited: get playlist by id method.

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -12,7 +12,7 @@ import Joi from "joi";
 
 const router = express.Router();
 
-// Create playlist
+// Route 1: Create playlist
 router.post(
   "/create",
   passport.authenticate("jwt", { session: false }),
@@ -31,6 +31,28 @@ router.post(
     };
     const playlist = await Playlist.create(playlistData);
     return res.status(200).send(playlist);
+  }
+);
+
+// Route 2: Get a playlist by ID
+// we will get the playlist ID as a route parameter and we will return teh playlist having that id
+// /something1/something2/something3 --> exact match
+// /something1/something2/something4 --> this will not call the api on the previus line
+// If we are doing /playlist/get/:playlistId (focus on teh :) --> this means that playlistId is now a variable to which we can assign any value
+// If you call anything of the format /playlist/get/asdvniuen (asdvniuen can be anything), this api is called
+// If you called /playlist/get/asdvniuen, the playlistId variable gets assigned teh value asdvniuen.
+router.get(
+  "/get/playlist/:playlistId",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    // This concept is called req.params
+    const playlistId = req.params.playlistId;
+    // I need to find a playlist with the _id = playlistId
+    const playlist = await Playlist.findOne({ _id: playlistId });
+    if (!playlist) {
+      return res.status(301).json({ err: "Invalid ID" });
+    }
+    return res.status(200).json(playlist);
   }
 );
 
@@ -133,17 +155,6 @@ router.get("/favourite", auth, async (req, res) => {
 router.get("/random", auth, async (req, res) => {
   const playlists = await Playlist.aggregate([{ $sample: { size: 10 } }]);
   res.status(200).send({ data: playlists });
-});
-
-// Get playlist by id
-router.get("/:id", [validObjectId, auth], async (req, res) => {
-  const playlist = await Playlist.findById(req.params.id);
-  if (!playlist) {
-    return res.status(404).send({ message: "Playlist not found" });
-  }
-
-  const songs = await Song.find({ _id: playlist.songs });
-  res.status(200).send({ data: { playlist, songs } });
 });
 
 // Get all playlists
