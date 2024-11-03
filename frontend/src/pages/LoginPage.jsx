f6acc505a4f07d0618ca4c38d3d7fa8a6c9cf151
JunCodera1 <minhtien0601200@gmail.com>
2024-11-03 22:59:18 +0700

    Added TextInput to LoginPage

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { GiMusicSpell } from "react-icons/gi";
 
 const LoginPage = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center">LoginPage</div>
+    <div className="w-full h-full flex flex-col items-center">
+      <div className="logo p-5 border-b border-solid border-gray-300 w-full flex justify-center">
+        <GiMusicSpell size={100} />
+      </div>
+      <div className="inputRegion">Text</div>
+    </div>
   );
 };
 
