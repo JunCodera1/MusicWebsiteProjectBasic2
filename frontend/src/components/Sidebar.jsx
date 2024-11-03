c82d061bb0a3af84cdba69fda25dae9501926541
BlackNoir <nhathuy1172005@gmai.com>
2024-11-03 17:13:53 +0700

    Update Sidebar component: adjust height and background color for improved aesthetics

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -25,18 +25,20 @@ export default function Sidebar() {
     }
   }, [isSmallScreen]);
 
+
+
   return (
     <Flex
       pos="sticky"
       left="5"
-      h="95vh"
+      h="88vh"
       marginTop="2.5vh"
       boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
       borderRadius={navSize == "small" ? "15px" : "30px"}
       w={navSize == "small" ? "75px" : { base: "75px", md: "200px" }}
       flexDir="column"
       justifyContent="space-between"
-      bg={useColorModeValue("gray.100", "gray.700")}
+      bg={useColorModeValue("gray.300", "gray.700")}
     >
       <Flex
         p="5%"
