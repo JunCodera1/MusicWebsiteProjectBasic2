d6ae89607f5070ebae704ce6633fb188b9fd04a1
JunCodera1 <minhtien0601200@gmail.com>
2024-11-03 10:30:32 +0700

    Updated: Error handling middleware passport-jwt.

--- backend/server.js
+++ backend/server.js
@@ -26,3 +26,27 @@ app.listen(PORT, () => {
   connectDB();
   console.log("Server started at http://localhost:" + PORT); // Start server
 });
+
+// Error handling middleware passport-jwt
+var JwtStrategy = require("passport-jwt").Strategy,
+  ExtractJwt = require("passport-jwt").ExtractJwt;
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = "secret";
+opts.issuer = "accounts.examplesoft.com";
+opts.audience = "yoursite.net";
+passport.use(
+  new JwtStrategy(opts, function (jwt_payload, done) {
+    User.findOne({ id: jwt_payload.sub }, function (err, user) {
+      if (err) {
+        return done(err, false);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+        // or you could create a new account
+      }
+    });
+  })
+);
