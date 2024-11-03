7cfdef0c440a02b6f88ba09347af42dbc9add813
JunCodera1 <minhtien0601200@gmail.com>
2024-11-04 00:29:18 +0700

    Deleted: "$".

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -9,10 +9,10 @@ router.get("/", auth, async (req, res) => {
   const search = req.query.search || "";
   if (search !== "") {
     const songs = await Song.find({
-      name: { $regex: search, $options: "i" },
+      name: { $regex: search, options: "i" },
     }).limit(10);
     const playlists = await Playlist.find({
-      name: { $regex: search, $options: "i" },
+      name: { $regex: search, options: "i" },
     }).limit(10);
     const result = { songs, playlists };
     res.status(200).send({ data: result });
