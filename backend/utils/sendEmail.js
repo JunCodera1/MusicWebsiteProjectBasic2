5e369a85d66ed5d3f05c226ad4281dc1cb5f48e4
JunCodera1 <minhtien0601200@gmail.com>
2024-11-17 16:16:07 +0700

    Added: Send email when forgot password.

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,17 +2,17 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async ({ email, subject, message }) => {
   const transporter = nodemailer.createTransport({
-    service: "Gmail", // Hoặc dịch vụ email bạn dùng, ví dụ: "Outlook", "Yahoo"
+    service: "Gmail",
     auth: {
       user: process.env.EMAIL_USER, // Email của bạn
-      pass: process.env.EMAIL_PASS, // Mật khẩu ứng dụng hoặc OAuth2 token
+      pass: process.env.EMAIL_PASS, // Mật khẩu ứng dụng
     },
   });
 
   const mailOptions = {
     from: process.env.EMAIL_USER, // Người gửi
     to: email, // Người nhận
-    subject, // Tiêu đề
+    subject, // Tiêu đề email
     text: message, // Nội dung email
   };
 
