446c8e34d8146a5f13757592ad35d08658e6c290
JunCodera1 <minhtien0601200@gmail.com>
2024-11-30 00:28:51 +0700

    remove clerk

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,25 +4,20 @@ import { ChakraProvider } from "@chakra-ui/react";
 import App from "./App";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { ClerkProvider } from "@clerk/clerk-react";
-
-const clerkId = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
-  <ClerkProvider publishableKey={clerkId}>
-    <React.StrictMode>
-      <ChakraProvider>
-        <App />
-        <ToastContainer
-          position="bottom-center"
-          autoClose={2000}
-          hideProgressBar={true}
-          closeButton={false}
-          theme="colored"
-          icon={false}
-        />
-      </ChakraProvider>
-    </React.StrictMode>
-  </ClerkProvider>
+  <React.StrictMode>
+    <ChakraProvider>
+      <App />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={2000}
+        hideProgressBar={true}
+        closeButton={false}
+        theme="colored"
+        icon={false}
+      />
+    </ChakraProvider>
+  </React.StrictMode>
 );
